1cd72910604a515a8fa70e83f3804c91b894705e
FireGrey <davidmaxwell77@gmail.com>
2016-04-21 22:54:29 -0400

    added stun/turn servers

--- js/webrtc.js
+++ js/webrtc.js
@@ -18,6 +18,7 @@ function roomListener(roomName, otherPeers) {
 
 function my_init(roomName) {
 	easyrtc.setSocketUrl(":8080");
+	//easyrtc.setOption("appIceServers", myIceServers);
 	easyrtc.setRoomOccupantListener(roomListener);
 	var connectSuccess = function(myId) {
 		console.log("My easyrtcid is " + myId);
@@ -34,6 +35,44 @@ function my_init(roomName) {
 		},
 		connectFailure
 	);
+	easyrtc.setIceUsedInCalls( {"iceServers": [
+{url:'stun:stun01.sipphone.com'},
+{url:'stun:stun.ekiga.net'},
+{url:'stun:stun.fwdnet.net'},
+{url:'stun:stun.ideasip.com'},
+{url:'stun:stun.iptel.org'},
+{url:'stun:stun.rixtelecom.se'},
+{url:'stun:stun.schlund.de'},
+{url:'stun:stun.l.google.com:19302'},
+{url:'stun:stun1.l.google.com:19302'},
+{url:'stun:stun2.l.google.com:19302'},
+{url:'stun:stun3.l.google.com:19302'},
+{url:'stun:stun4.l.google.com:19302'},
+{url:'stun:stunserver.org'},
+{url:'stun:stun.softjoys.com'},
+{url:'stun:stun.voiparound.com'},
+{url:'stun:stun.voipbuster.com'},
+{url:'stun:stun.voipstunt.com'},
+{url:'stun:stun.voxgratia.org'},
+{url:'stun:stun.xten.com'},
+{
+url: 'turn:numb.viagenie.ca',
+credential: 'muazkh',
+username: 'webrtc@live.com'
+},
+{
+url: 'turn:192.158.29.39:3478?transport=udp',
+credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+username: '28224511:1379330808'
+},
+{
+url: 'turn:192.158.29.39:3478?transport=tcp',
+credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+username: '28224511:1379330808'
+}
+]});
+
+
 }
 
 function performCall(easyrtcid) {
