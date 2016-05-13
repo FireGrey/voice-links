ab081960392d9bc206cc40dbeb71b58aab5b3d46
Sam Reid <Sam Reid>
2016-05-13 09:03:14 +0800

    ice config change

--- js/webrtc.js
+++ js/webrtc.js
@@ -22,6 +22,26 @@ function roomListener(roomName, otherPeers) {
 }
 
 function my_init(roomName) {
+	// Set STUN/TURN config
+	var ice_servers = [
+		{"url":"stun:stun:stun.l.google.com:19302"},
+		{url:'stun:stun1.l.google.com:19302'},
+		{url:'stun:stun2.l.google.com:19302'},
+		{url:'stun:stun3.l.google.com:19302'},
+		{url:'stun:stun4.l.google.com:19302'},
+		{
+			"url":"turn:numb.viagenie.ca",
+			"username":"muazkh",
+			"credential":"webrtc@live.com"
+		},
+		{
+			"url":"turn:192.158.29.39:3478?transport=tcp",
+			"username":"JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+			"credential":"28224511:1379330808"
+		}
+	];
+	easyrtc.setOption("appIceServers", ice_servers);
+
 	easyrtc.setSocketUrl(":8080");
 	easyrtc.setRoomOccupantListener(roomListener);
 	var connectSuccess = function(myId) {
@@ -33,45 +53,46 @@ function my_init(roomName) {
 	easyrtc.enableAudio(true);
 	easyrtc.enableVideo(false);
 
-	// Set STUN/TURN config
-	easyrtc.setIceUsedInCalls(
-		{"iceServers": [
-			{url:'stun:stun01.sipphone.com'},
-			{url:'stun:stun.ekiga.net'},
-			{url:'stun:stun.fwdnet.net'},
-			{url:'stun:stun.ideasip.com'},
-			{url:'stun:stun.iptel.org'},
-			{url:'stun:stun.rixtelecom.se'},
-			{url:'stun:stun.schlund.de'},
-			{url:'stun:stun.l.google.com:19302'},
-			{url:'stun:stun1.l.google.com:19302'},
-			{url:'stun:stun2.l.google.com:19302'},
-			{url:'stun:stun3.l.google.com:19302'},
-			{url:'stun:stun4.l.google.com:19302'},
-			{url:'stun:stunserver.org'},
-			{url:'stun:stun.softjoys.com'},
-			{url:'stun:stun.voiparound.com'},
-			{url:'stun:stun.voipbuster.com'},
-			{url:'stun:stun.voipstunt.com'},
-			{url:'stun:stun.voxgratia.org'},
-			{url:'stun:stun.xten.com'},
-			{
-			url: 'turn:numb.viagenie.ca',
-				credential: 'muazkh',
-				username: 'webrtc@live.com'
-			},
-			{
-			url: 'turn:192.158.29.39:3478?transport=udp',
-			credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-			username: '28224511:1379330808'
-			},
-			{
-			url: 'turn:192.158.29.39:3478?transport=tcp',
-			credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-			username: '28224511:1379330808'
-			}]
-		}
-	);
+
+	// I have re-iomplamented this - see above.
+	// easyrtc.setIceUsedInCalls(
+	// 	{"iceServers": [
+	// 		{url:'stun:stun01.sipphone.com'},
+	// 		{url:'stun:stun.ekiga.net'},
+	// 		{url:'stun:stun.fwdnet.net'},
+	// 		{url:'stun:stun.ideasip.com'},
+	// 		{url:'stun:stun.iptel.org'},
+	// 		{url:'stun:stun.rixtelecom.se'},
+	// 		{url:'stun:stun.schlund.de'},
+	// 		{url:'stun:stun.l.google.com:19302'},
+	// 		{url:'stun:stun1.l.google.com:19302'},
+	// 		{url:'stun:stun2.l.google.com:19302'},
+	// 		{url:'stun:stun3.l.google.com:19302'},
+	// 		{url:'stun:stun4.l.google.com:19302'},
+	// 		{url:'stun:stunserver.org'},
+	// 		{url:'stun:stun.softjoys.com'},
+	// 		{url:'stun:stun.voiparound.com'},
+	// 		{url:'stun:stun.voipbuster.com'},
+	// 		{url:'stun:stun.voipstunt.com'},
+	// 		{url:'stun:stun.voxgratia.org'},
+	// 		{url:'stun:stun.xten.com'},
+	// 		{
+	// 		url: 'turn:numb.viagenie.ca',
+	// 			credential: 'muazkh',
+	// 			username: 'webrtc@live.com'
+	// 		},
+	// 		{
+	// 		url: 'turn:192.158.29.39:3478?transport=udp',
+	// 		credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+	// 		username: '28224511:1379330808'
+	// 		},
+	// 		{
+	// 		url: 'turn:192.158.29.39:3478?transport=tcp',
+	// 		credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+	// 		username: '28224511:1379330808'
+	// 		}]
+	// 	}
+	// );
 
 	easyrtc.initMediaSource(
 		function(){
