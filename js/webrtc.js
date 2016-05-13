a2b9f6d7644edccb34695f3fb0cbd8e3f241cb4c
Sam Reid <Sam Reid>
2016-05-13 11:40:58 +0800

    work FUCKKKK

--- js/webrtc.js
+++ js/webrtc.js
@@ -36,20 +36,20 @@ function my_init(roomName) {
 
 	easyrtc.setIceUsedInCalls(
 		{"iceServers": [
-			{url:"stun:stun:stun.l.google.com:19302"},
+			{url:'stun:stun:stun.l.google.com:19302'},
 			{url:'stun:stun1.l.google.com:19302'},
 			{url:'stun:stun2.l.google.com:19302'},
 			{url:'stun:stun3.l.google.com:19302'},
 			{url:'stun:stun4.l.google.com:19302'},
 			{
-				"url":"turn:numb.viagenie.ca",
-				"username":"muazkh",
-				"credential":"webrtc@live.com"
+				url:"turn:numb.viagenie.ca",
+				credential:"webrtc@live.com",
+				username:"muazkh"
 			},
 			{
-				"url":"turn:192.158.29.39:3478?transport=tcp",
-				"username":"JZEOEt2V3Qb0y27GRntt2u2PAYA=",
-				"credential":"28224511:1379330808"
+				url:"turn:192.158.29.39:3478?transport=tcp",
+				credential:"JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+				username:"28224511:1379330808"
 			}
 		]}
 	);
